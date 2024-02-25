244179c9794a2aedeb461318f41b3ddde8a4802b
AYOUB EL ATMANI <ayoubelatmani50@gmail.com>
2024-02-25 20:35:41 +0100

    initialse project

---
+++ src/test/java/com/openaichatgpt/SpringBootOpenAiChatGptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openaichatgpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOpenAiChatGptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
